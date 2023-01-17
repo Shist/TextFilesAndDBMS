c300f3bb5f724308423fdb523a2e3a2f2b0a2e41
Shist <shist.pupust@mail.ru>
2023-01-17 19:13:48 +0300

    Добавлны кнопки для выполнения базовых функций

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,21 +29,83 @@ namespace TextFilesAndDBMS
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(250, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Сгенерировать файлы";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(250, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Объединить файлы в один";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(27, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(250, 30);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Импортировать файлы в таблицу СУБД";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(27, 158);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(250, 30);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Посчитать сумму всех целых чисел";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(27, 203);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(250, 30);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Посчитать медиану всех дробных чисел";
+            this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(302, 256);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Проект с текстовыми файлами и СУБД";
+            this.Text = "TextFilesAndDBMS";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
